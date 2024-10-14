eb08356176400038153f8d6e2ac6429d2dda35c9
KrokaKrola <harlamsan@gmail.com>
2024-10-14 19:23:56 +0300

    2530. Maximal Score After Applying K Operations

---
+++ src/daily/MaximumScoreAfterKOperations.java
@@ -0,0 +1,30 @@
+package daily;
+
+import java.util.PriorityQueue;
+
+public class MaximumScoreAfterKOperations {
+    public static void main(String[] args) {
+        System.out.println(maxKelements(new int[]{1, 10, 3, 3, 3}, 3));
+    }
+
+    public static long maxKelements(int[] nums, int k) {
+        long score = 0L;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+
+        for (int num : nums) {
+            maxHeap.add(num);
+        }
+
+        int operationsDone = 0;
+
+        while (operationsDone < k) {
+            int maxItem = maxHeap.poll();
+            score += maxItem;
+            maxItem = Math.ceilDiv(maxItem, 3);
+            maxHeap.add(maxItem);
+            operationsDone++;
+        }
+
+        return score;
+    }
+}
